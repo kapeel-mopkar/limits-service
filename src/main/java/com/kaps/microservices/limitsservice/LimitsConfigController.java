b04dd51b6161b32334af9b040d6c0b7f861a28a9
Kapeel Mopkar <kapeel_mopkar@persistent.com>
2019-11-29 15:12:50 +0530

    Added Hystrix support sample

--- src/main/java/com/kaps/microservices/limitsservice/LimitsConfigController.java
+++ src/main/java/com/kaps/microservices/limitsservice/LimitsConfigController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaps.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigController {
@@ -15,5 +16,15 @@ public class LimitsConfigController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
 		return new LimitConfiguration(config.getMaximum(), config.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-sample")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("FAULT_SIMULATION");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
 	}
 }
